a3825fae572a6aa533d71b146a269ed80273beb2
0leh-Kolesnyk <oleh.kolesnyk.dnepr@gmail.com>
2023-02-28 22:25:16 +0200

    Movies_App_v1 w_i_p - between result-not link-not title

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -10,34 +10,52 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import Slide from "./Slide";
 
-export default ({ list }) => {
-  //   const Movie = (list) => {
-  //     list.map((item) => {
-  //       const key = item.id;
-  //     });
-  //   };
+export default function SwiperMovies({ list }) {
   return (
     <Swiper
+      className="swiperShadow"
       // install Swiper modules
       modules={[Scrollbar, A11y]}
       spaceBetween={30}
       slidesPerView={7.7}
-      // navigation
+      navigation
       // pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       // onSwiper={(swiper) => console.log(swiper)}
       // onSlideChange={() => console.log("slide change")}
     >
-      {list.map((movie) => (
-        <SwiperSlide>
-          <Slide
-            key={movie.id}
-            {...movie}
-            title={movie.title}
-            moviePosterImg={movie.poster_path}
-          />
-        </SwiperSlide>
-      ))}
+      {list.map((movie) => {
+        const key = movie.id;
+        console.log(movie.title);
+        return (
+          <SwiperSlide
+          // {/* <Slide */}
+          // {/* {...movie} */}
+          // {/* movie={movie} */}
+          // title={movie.title}
+          // {/* moviePosterImg={movie.poster_path} */}
+          // {/* /> */}
+          >
+            <>
+              <div className="">
+                <a href="#" className="">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    title={movie.title}
+                    className="imageSlide rounded-3xl max-h-56"
+                  />
+                </a>
+
+                <div className="titleSlide text-base font-bold pt-6 px-2">
+                  <a href="#" className="" title={movie.title}>
+                    {movie.title}
+                  </a>
+                </div>
+              </div>
+            </>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
-};
+}
